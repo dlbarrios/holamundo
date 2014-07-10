22e271b2aa3e8b7d29f8a5b7dacb723d3696ea82
DLBarrios <DLBarrios@MOVIL-73SFZN1.administracion.local>
2014-07-09 18:27:56 -0600

    Se muestra hola mundo. Ahora en repositorio Git.

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -18,7 +18,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         
         // usar la multiplicacion
-        System.out.println("hola mundo");
+        System.out.println("hola mundo, ahora en Git");
         
        
     }
